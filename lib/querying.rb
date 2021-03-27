26f5ecd68a0cfa9f29bf0e05f546eca952bcf013
maryamzain123 <smaryam.zain@gmail.com>
2021-03-26 22:47:39 -0500

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,24 +1,38 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT title, year FROM books
+  WHERE series_id = 1
+  ORDER BY year ASC;"
+ 
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT name, motto FROM characters ORDER BY LENGTH(motto) DESC LIMIT 1;"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, COUNT(species) FROM characters GROUP BY species ORDER BY species DESC LIMIT 1 ;"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+
+  "SELECT authors.name, subgenres.name FROM authors
+    JOIN series ON series.author_id = authors.id 
+   JOIN subgenres ON series.subgenre_id = subgenres.id;"
+
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title FROM series
+  JOIN series ON series.author_id = authors.id
+  JOIN characters on characters.author_id = authors.id
+  GROUP BY MAX(characters.name)
+  ORDER BY characters.name DESC;"
+ 
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, books.title FROM characers
+  GROUP BY Characters.name ORDER BY books DESC and ORDER BY characters.name ASC;"
+ 
 end
